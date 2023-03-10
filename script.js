7ab4df39dc02ff1b591648a46003b902d8788ff3
Ravi <ravkchauhan1aip@gmail.com>
2023-03-10 23:25:29 +0530

    update

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ textarea.focus()
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
 
-    
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
